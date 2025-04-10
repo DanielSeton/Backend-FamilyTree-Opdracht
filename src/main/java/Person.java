098ddfa7a07991241ad5431f801bd5b775e65fb0
DanielSeton <danielseton@live.nl>
2025-04-10 17:30:46 +0200

    project opgezet

---
+++ src/main/java/Person.java
@@ -0,0 +1,78 @@
+import java.util.List;
+
+public class Person {
+    private String name;
+    private String middleName;
+    private String lastName;
+    private String sex;
+    private int age;
+    private Person mother;
+    private Person father;
+    private List<Person> sibling;
+    private List<Person> children;
+    private List<Pet> pet;
+
+    public Person(String name, String lastName, int age, String sex) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public Person(String name, String middleName, String lastName, int age, String sex) {
+        this.name = name;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public List<Person> getSibling() {
+        return sibling;
+    }
+
+    public void setSibling(List<Person> sibling) {
+        this.sibling = sibling;
+    }
+
+    public List<Person> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Person> children) {
+        this.children = children;
+    }
+
+    public List<Pet> getPet() {
+        return pet;
+    }
+
+    public void setPet(List<Pet> pet) {
+        this.pet = pet;
+    }
+
+    public Person getMother() {
+        return mother;
+    }
+
+    public void setMother(Person mother) {
+        this.mother = mother;
+    }
+
+    public Person getFather() {
+        return father;
+    }
+
+    public void setFather(Person father) {
+        this.father = father;
+    }
+
+    public void addParents(){
+        setFather(father);
+        setMother(mother);
+    }
+
+    public void addChild(){
+
+    }
+}
